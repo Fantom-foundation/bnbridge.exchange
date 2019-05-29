6edfffef5e00c163cacbcd58a1e000b337eb0d22
anton <anton@mfin.co.za>
2019-05-29 12:06:38 +0200

    Commit.
    Signed-off-by: anton <anton@mfin.co.za>

--- bnbridge/src/config/production.config.js
+++ bnbridge/src/config/production.config.js
@@ -1,6 +1,6 @@
 const config = {
-  apiUrl: "http://ec2-52-91-230-135.compute-1.amazonaws.com:8000",
-  apiToken: "ZTgwMTY1NjkzZjAyOTk1N2VjNDQ4MjBhNGRiODJiMGI1NjI5YjM2YjJkNjc1YjVhYjE0YmEwNTBhMDFiNDk3ZDpmYmM3MWMyOTRmOWE4N2VlM2QzMmVkZDVkNjExNTE4MTFlNDRmNzc0NDgzNzY4OWVmYWRkYmJiOWY3NjgxYzA5",
+  apiUrl: "",
+  apiToken: "",
   explorerURL: "https://explorer.binance.org/tx/"
 };
 
